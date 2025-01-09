e5e87a1d9e72fde230b6305760d1a37e20a8b153
Watt09 <161873426+Watt09@users.noreply.github.com>
2025-01-09 18:00:26 -0300

    Update mysql.mjs

--- database/mysql.mjs
+++ database/mysql.mjs
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const conexao = new Sequelize({
+    host: 'dpg-cu03d15ds78s73euaojg-a',
+    port: '5432',
     database: 'projetofinal_1zt5',
     username: 'projetofinal_1zt5_user',
     password: 'hkyqmm4SXX9D1YHcST6SPoLqDDom8SK5',
-    dialect: 'mysql'
+    dialect: 'postgres'
 })
 
 export default conexao;
