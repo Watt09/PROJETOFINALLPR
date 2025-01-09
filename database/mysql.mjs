6fd9ed29264519336f721d9aad954925f33460ec
Watt09 <161873426+Watt09@users.noreply.github.com>
2025-01-09 17:58:22 -0300

    Update mysql.mjs

--- database/mysql.mjs
+++ database/mysql.mjs
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
 const conexao = new Sequelize({
-    database: 'bdlpr1',
-    username: 'root',
-    password: 'root',
+    database: 'projetofinal_1zt5',
+    username: 'projetofinal_1zt5_user',
+    password: 'hkyqmm4SXX9D1YHcST6SPoLqDDom8SK5',
     dialect: 'mysql'
 })
 
