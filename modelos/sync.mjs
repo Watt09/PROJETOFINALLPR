e61ec39d9efcc33c03870eed4a099618131caf6c
Watt09 <161873426+Watt09@users.noreply.github.com>
2025-01-10 07:45:12 -0300

    Update sync.mjs

--- modelos/sync.mjs
+++ modelos/sync.mjs
@@ -14,7 +14,7 @@ Produto.belongsTo(Categoria);
 Categoria.hasMany(Produto);
 
 
-conexao.sync({ alter: true })
+conexao.sync({ force: true })
   .then(() => console.log("Banco sincronizado com sucesso!"))
   .catch(err => console.error("Erro ao sincronizar banco:", err));
 
