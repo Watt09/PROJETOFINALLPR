aeae2550d7d52ff7623d5f0771fb58276b45d6a7
Watt09 <161873426+Watt09@users.noreply.github.com>
2025-01-12 20:42:09 -0300

    dnv

--- database/mysql.mjs
+++ database/mysql.mjs
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
-import Categoria from './modelos/categoria.mjs';
-import Cliente from './modelos/cliente.mjs';
-import Funcionario from './modelos/funcionario.mjs';
-import Lucrocusto from './modelos/lucrocusto.mjs';
-import Produto from './modelos/produto.mjs';
-import Pedido from './modelos/pedido.mjs';
+import Categoria from '../modelos/categoria.mjs';
+import Cliente from '../modelos/cliente.mjs';
+import Funcionario from '../modelos/funcionario.mjs';
+import Lucrocusto from '../modelos/lucrocusto.mjs';
+import Produto from '../modelos/produto.mjs';
+import Pedido from '../modelos/pedido.mjs';
 
 const conexao = new Sequelize({
     host: 'dpg-cu03d15ds78s73euaojg-a',
