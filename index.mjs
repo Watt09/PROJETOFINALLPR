f0792413bc832d566c46e89ade79275087a1e40c
Watt09 <161873426+Watt09@users.noreply.github.com>
2025-01-12 11:13:29 -0300

    a

--- index.mjs
+++ index.mjs
@@ -4,6 +4,7 @@ import rotas_produto from './rotas/rotas_produto.mjs';
 import rotas_pedido from './rotas/rotas_pedido.mjs';
 import rotas_categoria from './rotas/rotas_categoria.mjs';
 import rotas_funcionario from './rotas/rotas_funcionario.mjs';
+import rotas_lucrocusto from './rotas/rotas_lucrocusto.mjs';
 import conexao from './modelos/sync.mjs'
 
 const app = express();
@@ -14,12 +15,14 @@ app.use('/produto', rotas_produto);
 app.use('/categoria', rotas_categoria);
 app.use('/pedido', rotas_pedido);
 app.use('/funcionario', rotas_funcionario);
+app.use('/lucrocusto', rotas_lucrocusto);
 app.use(express.static('./views/clientes'));
 app.use(express.static('./views/categoria'));
 app.use(express.static('./views/produto'));
 app.use(express.static('./views/pedido'));
 app.use(express.static('./views/pedido'));
 app.use(express.static('./views/funcionario'));
+app.use(express.static('./views/lucrocusto'));
 app.use(express.static('./views'));
 
 app.listen(80, function () {
